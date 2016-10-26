774c31f4533dfb895911b65cb00135a7d4ce6e5d
yangxun1200 <yangxun1200@gmail.com>
2016-10-26 10:23:43 -0400

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -16,7 +16,7 @@ function loadMapScenario() {
     map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
         credentials: 'Ak3Ti03DdEJJLxfmHLqYpir33prmxiwRVYqwOsERuP3EepvEddqjGrL4dTLnDVK3'
     });
-}                }
+}                
 
 
 // 1. Define pokemon data format, create mock pokemon data
